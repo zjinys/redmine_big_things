91624659a8a9380bdcb8ea53d6cbaabd839a633c
pp <zjinys@gmail.com>
2010-07-19 19:38:31 +0800

    initial commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_big_things do
+  name 'Redmine Big Things plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
